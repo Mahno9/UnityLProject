16f8ff6390df535f0d574f41ab919ce93a45eee5
Mahno9 <mahno32@mail.ru>
2026-06-11 19:11:42 +0500

    Not rotate in case of death

--- Assets/_Project/Develop/Runtime/Gameplay/Features/RotationFeature/AlongMovementRotationSystem.cs
+++ Assets/_Project/Develop/Runtime/Gameplay/Features/RotationFeature/AlongMovementRotationSystem.cs
@@ -1,5 +1,6 @@
 using _Project.Develop.Runtime.Gameplay.EntitiesCore;
 using _Project.Develop.Runtime.Gameplay.EntitiesCore.Systems;
+using _Project.Develop.Runtime.Utilities.Conditions;
 using _Project.Develop.Runtime.Utilities.Reactive;
 
 using UnityEngine;
@@ -11,16 +12,21 @@ namespace _Project.Develop.Runtime.Gameplay.Features.RotationFeature
         private ReactiveVariable<Vector3>    _moveDirection;
         private ReactiveVariable<float>      _rotationSpeed;
         private ReactiveVariable<Quaternion> _rotation;
+        private ICompositeCondition          _canRotate;
 
         public void OnInit(Entity entity)
         {
             _moveDirection = entity.MoveDirection;
             _rotationSpeed = entity.RotationSpeed;
             _rotation = entity.Rotation;
+            _canRotate = entity.CanRotate;
         }
 
         public void OnUpdate(float deltaTime)
         {
+            if (_canRotate.Evaluate() == false)
+                return;
+
             Vector3 targetLookDirection = _moveDirection.Value.normalized;
 
             if (targetLookDirection == Vector3.zero)
